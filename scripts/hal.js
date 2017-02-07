7072747a805516afe3c2496c461ab53fc2ff432d
simontegg <simon@enspiral.com>
2017-02-08 11:08:23 +1300

    es5strings

--- scripts/hal.js
+++ scripts/hal.js
@@ -2,7 +2,7 @@ module.exports = function (robot) {
   robot.respond(/open the pod bay doors/i, function (res) { 
     var name = res.message.user.profile.first_name
 
-    res.send(`I'm sorry ${name}...`)
+    res.send("I'm sorry " + name + "...")
 
     setTimeout(function () {
       res.send("I'm afraid I can't do that")
