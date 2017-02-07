0bf3d9db6a72633b1591632da19528f6d61f5418
simontegg <simon@enspiral.com>
2017-02-08 11:04:52 +1300

    es5

--- scripts/hal.js
+++ scripts/hal.js
@@ -1,10 +1,10 @@
-module.exports = (robot) => {
-  robot.respond(/open the pod bay doors/i, (res) => { 
-    const name = res.message.user.profile.first_name
+module.exports = function (robot) {
+  robot.respond(/open the pod bay doors/i, function (res) { 
+    var name = res.message.user.profile.first_name
 
     res.send(`I'm sorry ${name}...`)
 
-    setTimeout(() => {
+    setTimeout(function () {
       res.send("I'm afraid I can't do that")
     }, 3000)
 
